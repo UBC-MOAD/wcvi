5f0dd96ec82e6a5be74bff4cca325deed62053ab
sasahu <ssahu@eoas.ubc.ca>
2017-05-19 10:44:58 -0700

    Started the grid transformation routines from JP to WCVI to make BC

---
+++ grid/grid_alignment.py
@@ -0,0 +1,42 @@
+import numpy as np
+
+
+def calculate_initial_compass_bearing(pointA, pointB):
+    ''' The bearing between two points is calculated.
+       
+    θ = atan2( sin Δλ ⋅ cos φ2 , cos φ1 ⋅ sin φ2 − sin φ1 ⋅ cos φ2 ⋅ cos Δλ )
+    φ1,λ1 is the start point
+    φ2,λ2 the end point 
+    
+    :Parameters:
+      - `pointA: The tuple representing the latitude/longitude for the
+        first point. Latitude and longitude must be in decimal degrees
+      - `pointB: The tuple representing the latitude/longitude for the
+        second point. Latitude and longitude must be in decimal degrees
+    :Returns:
+      The bearing in degrees
+    :Returns Type:
+      float
+    '''
+    
+    if (type(pointA) != tuple) or (type(pointB) != tuple):
+        raise TypeError("Only tuples are supported as arguments")
+
+    lat1 = np.radians(pointA[0])
+    lat2 = np.radians(pointB[0])
+    
+    diffLong = np.radians(pointB[1] - pointA[1])
+
+    x = np.sin(diffLong) * np.cos(lat2)
+    y = np.cos(lat1) * np.sin(lat2) - (np.sin(lat1)
+            * np.cos(lat2) * np.cos(diffLong))
+
+    initial_bearing = np.arctan2(x, y)
+
+    # Now we have the initial bearing but math.atan2 return values
+    # from -180° to + 180° which is not what we want for a compass bearing
+    # The solution is to normalize the initial bearing as shown below
+    initial_bearing = np.degrees(initial_bearing)
+    compass_bearing = (initial_bearing + 360) % 360
+
+    return compass_bearing
